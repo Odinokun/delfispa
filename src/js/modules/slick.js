91c783e6d009ff9c9253116bc5f02f60f288c43b
odinokun <odinokun@gmail.com>
2018-04-10 13:04:10 +0300

    did catalog slider

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -2,6 +2,7 @@ module.exports = function() {
 
   // begin Slick slider
 
+  // begin top-slider in index.html
   $('#top-slider').slick({
     dots: true,
     infinite: true,
@@ -11,6 +12,40 @@ module.exports = function() {
     autoplay: true,
     cssEase: 'linear'
   });
+  // end top-slider in index.html
+
+  // begin catalog-slider in index.html
+  $('#catalog-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1401,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1001,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 701,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  // end catalog-slider in index.html
 
   // end Slick slider
 
