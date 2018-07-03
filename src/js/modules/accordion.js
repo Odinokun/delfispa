ee5e1d0e04fa23b76f66b2202194aaaf62af0c4b
odinokun <odinokun@gmail.com>
2018-07-03 14:22:10 +0300

    commit

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -2,12 +2,17 @@ module.exports = function() {
 
   // begin accordion
 
-  $('.faq-accordion__row').on('click', function () {
-    $('.faq-accordion__row').removeClass('active');
-    $(this).addClass('active');
+  // $('.faq-accordion__row').on('click', function () {
+  //   $('.faq-accordion__row').removeClass('active');
+  //   $(this).addClass('active');
+  //
+  //   $('.faq-accordion__answer').slideUp();
+  //   $(this).find('.faq-accordion__answer').slideDown();
+  // });
 
-    $('.faq-accordion__answer').slideUp();
-    $(this).find('.faq-accordion__answer').slideDown();
+  $('.faq-accordion__row').on('click', function () {
+    $(this).toggleClass('active');
+    $(this).find('.faq-accordion__answer').slideToggle();
   });
 
   // end accordion
