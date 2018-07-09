0fd42568be9b709a0a93a6d7ea65cfbcb515f23c
odinokun <odinokun@gmail.com>
2018-07-10 00:26:49 +0300

    edit accordeon

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -10,9 +10,21 @@ module.exports = function() {
   //   $(this).find('.faq-accordion__answer').slideDown();
   // });
 
+  // $('.faq-accordion__row').on('click', function () {
+  //   $(this).toggleClass('active');
+  //   $(this).find('.faq-accordion__answer').slideToggle();
+  // });
+
   $('.faq-accordion__row').on('click', function () {
-    $(this).toggleClass('active');
-    $(this).find('.faq-accordion__answer').slideToggle();
+    if ($(this).hasClass('active')) {
+      $('.faq-accordion__row').removeClass('active');
+      $(this).find('.faq-accordion__answer').slideUp();
+    } else {
+      $('.faq-accordion__row').removeClass('active');
+      $(this).addClass('active');
+      $('.faq-accordion__answer').slideUp();
+      $(this).find('.faq-accordion__answer').slideDown();
+    }
   });
 
   // end accordion
